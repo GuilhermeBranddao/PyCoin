b529d1d4ca348f7ea4e4b65999ce219808921e28
Guilherme Brandão <guilhermebranddao@gmail.com>
2024-11-21 17:46:25 -0300

    refactor: classe Blockchain

--- pycoin/app.py
+++ pycoin/app.py
@@ -2,8 +2,19 @@ from uuid import uuid4
 
 from fastapi import FastAPI, HTTPException, Request
 
-from pycoin.blockchain.pycoin import Blockchain
-from pycoin.blockchain.tool_blockchain import is_chain_valid, load_nodes
+from pycoin.blockchain.pycoin import BlockchainInitializer
+
+BlockchainInitializer()
+
+from pycoin.blockchain.tool_blockchain import (
+    add_node,
+    check_progagate_blockchain,
+    create_block,
+    is_chain_valid,
+    load_chain,
+    load_nodes,
+    replace_chain,
+)
 from pycoin.routers import wallet
 from pycoin.schemas import NodeListRequest
 from pycoin.settings import Settings
@@ -11,7 +22,7 @@ from pycoin.settings import Settings
 settings = Settings()
 app = FastAPI()
 node_address = str(uuid4()).replace('-', '')
-blockchain = Blockchain()
+
 
 # Wallet and transactions
 
@@ -21,31 +32,34 @@ app.include_router(wallet.router)
 # Miners routes
 @app.get('/mine_block')
 def mine_block():
-    block = blockchain.create_block()
+    block = create_block()
     return block
 
 
 @app.get('/get_actual_chain')
 def get_actual_chain():
+    chain = load_chain()
     response = {
         'message': 'Nós presente na rede atualmente',
-        'actual_chain': blockchain.blockchain,
+        'actual_chain': chain,
     }
     return response
 
 
 @app.get('/replace_chain')
-def replace_chain():
-    is_chain_replaced = blockchain.replace_chain()
+def replace_the_chain():
+    is_chain_replaced = replace_chain()
+    chain = load_chain()
+
     if is_chain_replaced:
         response = {
             'message': 'Nós atualizados com sucesso',
-            'new_chain': blockchain.blockchain,
+            'new_chain': chain,
         }
     else:
         response = {
             'message': 'Não é necessário atualizar os nós da rede.',
-            'actual_chain': blockchain.blockchain,
+            'actual_chain': chain,
         }
     return response
 
@@ -57,17 +71,20 @@ def connect_node(request: NodeListRequest):
     Exemplo de uso:
         - Enviar uma lista de nós para conectar.
     """
-    nodes = request.nodes
-    if not nodes:
+
+    nodes_request = request.nodes
+
+    if not nodes_request:
         raise HTTPException(status_code=400, detail="A lista de nós não pode ser vazia")
 
-    for node in nodes:
-        # Simula a adição do nó ao blockchain
-        blockchain.add_node(f"{node.node_address}:{node.port}")
+    possible_new_nodes = [f"{node.node_address}:{node.port}" for node in nodes_request]
 
+    add_node(possible_new_nodes=possible_new_nodes)
+
+    nodes = load_nodes()
     response = {
         'message': 'Conexão realizada com sucesso entre os seguintes nós:',
-        'total_nodes': list(blockchain.nodes)
+        'total_nodes': list(nodes)
     }
 
     return response
@@ -75,7 +92,8 @@ def connect_node(request: NodeListRequest):
 
 @app.get('/is_valid')
 def is_valid():
-    is_valid = is_chain_valid(blockchain.blockchain)
+    chain = load_chain()
+    is_valid = is_chain_valid(chain)
     if is_valid:
         response = {'message': 'Blockchain Válido'}
     else:
@@ -85,8 +103,9 @@ def is_valid():
 
 @app.get('/get_chain')
 def get_chain():
-    response = {'chain': blockchain.blockchain,
-                'length': len(blockchain.blockchain)}
+    chain = load_chain()
+    response = {'chain': chain,
+                'length': len(chain)}
     return response
 
 # Others
@@ -103,7 +122,7 @@ def get_my_nodes():
 async def new_blockchain(request: Request):
     json = await request.json()
 
-    response = blockchain.check_progagate_blockchain(
+    response = check_progagate_blockchain(
         new_blockchain=json['chain'],
         nodes_updated=json['nodes_updated'])
 
